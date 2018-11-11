bf266cc680dfa5367d7b4b534b720ee7b719b4f6
972208372 <38084904+972208372@users.noreply.github.com>
2018-11-10 22:28:24 -0500

    select image

--- app/src/main/java/com/purdue/waifu2x/waifu2x/MainActivity.java
+++ app/src/main/java/com/purdue/waifu2x/waifu2x/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,26 @@ public class MainActivity extends AppCompatActivity {
         Intent pickPhoto = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(pickPhoto , 1);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        imageView1=(ImageView) findViewById(R.id.imageView);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageView1.setImageURI(selectedImage);
+                }
 
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageView1.setImageURI(selectedImage);
+                }
+                break;
+        }
     }
 
     public void convert(View view) {
